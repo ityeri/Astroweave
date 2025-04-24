2ddeaff4242a91c294f982497eacb6bb1f407acd
it <yeongbin0909@naver.com>
2025-04-24 19:46:34 +0900

    utils 모듈에 있던 Ktor Client 종속성을 core 로 이동

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -16,6 +16,10 @@ dependencies {
 
     implementation("org.jsoup:jsoup:1.16.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
+    implementation("io.ktor:ktor-client-core:2.3.4")
+    implementation("io.ktor:ktor-client-cio:2.3.4")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.4")
 }
 
 tasks.test {
--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -11,10 +11,6 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jsoup:jsoup:1.16.1")
-
-    implementation("io.ktor:ktor-client-core:2.3.4")
-    implementation("io.ktor:ktor-client-cio:2.3.4")
-    implementation("io.ktor:ktor-client-content-negotiation:2.3.4")
 }
 
 tasks.test {
