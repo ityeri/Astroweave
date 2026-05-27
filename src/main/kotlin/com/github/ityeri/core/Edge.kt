e7378ad4b59ae3a28f0a911fae1787e32bd84563
ityeri <ityeri9@gmail.com>
2026-05-27 20:01:17 +0900

    add: URLGraphStore interface

---
+++ src/main/kotlin/com/github/ityeri/core/Edge.kt
@@ -0,0 +1,5 @@
+package com.github.ityeri.core
+
+import io.mola.galimatias.URL
+
+data class Edge(val from: URL, val to: URL)
---
+++ src/main/kotlin/com/github/ityeri/core/URLGraphStore.kt
@@ -0,0 +1,11 @@
+package com.github.ityeri.core
+
+import io.mola.galimatias.URL
+
+interface URLGraphStore {
+    fun add(url: URL)
+    fun remove(url: URL): Boolean
+    fun includes(url: URL): Boolean
+    fun addEdge(edge: Edge)
+    fun removeEdge(edge: Edge): Boolean
+}
