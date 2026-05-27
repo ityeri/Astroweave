d252ba2349ee9821c778f1c3bba21bd2307f996a
ityeri <ityeri9@gmail.com>
2026-05-27 20:01:46 +0900

    add: Temporary URLGraphStoreMemoryImpl

---
+++ src/main/kotlin/com/github/ityeri/core/impl/memory/URLGraphStoreMemoryImpl.kt
@@ -0,0 +1,26 @@
+package com.github.ityeri.core.impl.memory
+
+import com.github.ityeri.core.Edge
+import com.github.ityeri.core.URLGraphStore
+import io.mola.galimatias.URL
+
+class URLGraphStoreMemoryImpl : URLGraphStore {
+    val urls: MutableSet<URL> = hashSetOf()
+    val edges: MutableSet<Edge> = hashSetOf()
+
+    override fun add(url: URL) {
+        urls.add(url)
+    }
+    override fun remove(url: URL): Boolean {
+        return urls.remove(url)
+    }
+    override fun includes(url: URL): Boolean {
+        return urls.contains(url)
+    }
+    override fun addEdge(edge: Edge) {
+        edges.add(edge)
+    }
+    override fun removeEdge(edge: Edge): Boolean {
+        return edges.remove(edge)
+    }
+}
