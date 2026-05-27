1dabed14c5baac17eba293b7cc9f615d98616e8c
ityeri <ityeri9@gmail.com>
2026-05-27 20:02:52 +0900

    edit: Replace Main.kt code to koin starting code

--- src/main/kotlin/com/github/ityeri/core/Main.kt
+++ src/main/kotlin/com/github/ityeri/core/Main.kt
@@ -1,60 +1,25 @@
 package com.github.ityeri.core
 
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import java.sql.Connection
-import java.sql.DriverManager
-import kotlinx.coroutines.*
+import io.klogging.config.ANSI_CONSOLE
+import io.klogging.config.ANSI_INFO
+import io.klogging.config.loggingConfiguration
+import kotlinx.coroutines.runBlocking
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import org.koin.logger.SLF4JLogger
+import org.koin.mp.KoinPlatform
 
-fun main(): Unit = runBlocking {
+fun main() {
+    println("main: Starting...")
 
-    val taskerCount = 1000
-
-    val jdbcUrl = "jdbc:mysql://localhost:3306/" +
-        "astroweave_db" +
-        "?useSSL=false" +
-        "&serverTimezone=UTC" +
-        "&allowPublicKeyRetrieval=true"
-
-    val username = "root"
-    val password = "root"
-
-    val connection: Connection = DriverManager.getConnection(jdbcUrl, username, password)
-    connection.autoCommit = false
-
-    val httpClient = HttpClient(CIO)
-
-    val taskerList = mutableListOf<Tasker>()
-    val jobs = mutableListOf<Job>()
-
-    for (i in 0 until taskerCount) {
-        val tasker = Tasker(connection, httpClient)
-        taskerList.add(tasker)
-        println("새 작업 시작")
-
-        jobs.add(
-            launch {
-                val taskerId = taskerList.indexOf(tasker)
-
-                try {
-                    while (true) {
-                        println("$taskerId 번 작업자 주기 시작")
-                        tasker.runCycle(5)
-                        println("$taskerId 번 작업자 주기 끝")
-                        delay(10)
-                    }
-                }
-                catch (e: Exception) {
-                    println("$taskerId 번 작업자가 다음과 같은 오류로 중단됨: ")
-                    println(e)
-                }
-            }
-        )
+    startKoin {
+        logger(SLF4JLogger(Level.INFO))
+        modules(appModule)
     }
 
-    for (job in jobs) {
-        job.join()
+    runBlocking {
+        println("main: Coroutine started")
+        val crawler = KoinPlatform.getKoin().get<Crawler>()
+        crawler.run()
     }
-
-    httpClient.close()
 }
