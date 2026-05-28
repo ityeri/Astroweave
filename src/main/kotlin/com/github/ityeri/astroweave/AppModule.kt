7f393469b8f99038439d7efb747cd3b312dcfbde
ityeri <ityeri9@gmail.com>
2026-05-28 16:03:31 +0900

    add: HttpClient timeout settings

--- src/main/kotlin/com/github/ityeri/astroweave/AppModule.kt
+++ src/main/kotlin/com/github/ityeri/astroweave/AppModule.kt
@@ -1,13 +1,39 @@
 package com.github.ityeri.astroweave
 
-import com.github.ityeri.astroweave.impl.memory.URLGraphStoreMemoryImpl
+import com.github.ityeri.astroweave.store.url.impl.memory.UrlGraphStoreMemoryImpl
+import com.github.ityeri.astroweave.store.url.UrlGraphStore
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.cio.endpoint
+import io.ktor.client.plugins.HttpTimeout
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import org.koin.dsl.module
 
 val appModule = module {
-    single<URLGraphStore> {
-        URLGraphStoreMemoryImpl()
+    single<UrlGraphStore> {
+        UrlGraphStoreMemoryImpl()
+    }
+    single<HttpClient> {
+        HttpClient(CIO) {
+            engine {
+                endpoint {
+                    connectTimeout = Long.MAX_VALUE
+                    socketTimeout = Long.MAX_VALUE
+                }
+            }
+            install(HttpTimeout) {
+                requestTimeoutMillis = 60_000
+                connectTimeoutMillis = Long.MAX_VALUE
+                socketTimeoutMillis = Long.MAX_VALUE
+            }
+        }
     }
     single<Crawler> {
-        Crawler(graphStore = get(), tickInterval = 0.1f)
+        Crawler(
+            graphStore = get(),
+            client = get(),
+            scope = CoroutineScope(Dispatchers.IO)
+        )
     }
 }
