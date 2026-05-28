2dc12e463749eedb26acd24f0595d5b8f88442b4
ityeri <ityeri9@gmail.com>
2026-05-28 16:03:11 +0900

    add: Initial url request at start of main function

--- src/main/kotlin/com/github/ityeri/astroweave/Main.kt
+++ src/main/kotlin/com/github/ityeri/astroweave/Main.kt
@@ -1,5 +1,7 @@
 package com.github.ityeri.astroweave
 
+import io.ktor.client.HttpClient
+import io.mola.galimatias.URL
 import kotlinx.coroutines.runBlocking
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
@@ -16,7 +18,17 @@ fun main() {
 
     runBlocking {
         println("main: Coroutine started")
-        val crawler = KoinPlatform.getKoin().get<Crawler>()
-        crawler.run()
+
+        val koin = KoinPlatform.getKoin()
+        val crawler = koin.get<Crawler>()
+        crawler.addUrlTask(
+            UrlTask(null, URL.parse("https://google.com"))
+        )
+
+        try {
+            crawler.run()
+        } finally {
+            koin.get<HttpClient>().close()
+        }
     }
 }
